c973f0adebf02aa1e80feee3841c8e8099c99931
Jan Mittendorf <jan.mittendorf@freenet.de>
2023-12-05 17:42:34 +0100

    Add script for adding satellite datasets to JSON

---
+++ source/utility.py
@@ -0,0 +1,29 @@
+from configparser import ConfigParser
+from pathlib import Path
+
+CONFIG_PATH = "config.cfg"
+
+type SatelliteDataset = dict[str, str]
+
+
+class Config:
+    def __init__(self, config_path: str) -> None:
+        cfg_parser = ConfigParser()
+        cfg_parser.read(config_path)
+
+        self.satellite_datasets_json_path = Path(
+            cfg_parser["Paths"]["satellite_datasets_json"]
+        )
+        self.json_indent = cfg_parser["Misc"].getint("json_indent")
+
+
+config = Config(CONFIG_PATH)
+
+
+def user_confirm(message: str) -> bool:
+    reply = input(f"{message} (y/n): ")
+
+    while reply.lower() not in ("y", "n", "yes", "no"):
+        reply = input("Please reply with 'yes'/'y' or 'no'/'n': ")
+
+    return reply.lower() in ("y", "yes")
