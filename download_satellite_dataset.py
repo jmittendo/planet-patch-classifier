1757776e258ea504732b75aecda624246e5877fd
Jan Mittendorf <jan.mittendorf@freenet.de>
2023-12-06 16:26:42 +0100

    Continue implementation VEX VMC UV2 download works now

--- download_satellite_dataset.py
+++ download_satellite_dataset.py
@@ -11,60 +11,87 @@ FILE_NAME_FILTER = "UV2"
 OUTPUT_DIR = "datasets/satellite-datasets/vex-test"
 
 
-def main():
+def main() -> None:
     dir_url_stripped = DIRECTORY_URL.rstrip("/")
 
     mission_dir_names = [
         href.rstrip("/")
         for href in get_hrefs(
-            DIRECTORY_URL, dir_only=True, href_filters=[SUB_URL_FILTER]
+            DIRECTORY_URL, dir_only=True, positive_filters=[SUB_URL_FILTER]
         )
     ]
 
-    for mission_dir_name in mission_dir_names:
+    for mission_dir_name in tqdm(
+        mission_dir_names, desc="Full download progress", leave=False
+    ):
         mission_dir_url = f"{dir_url_stripped}/{mission_dir_name}"
+        mission_dir_path = Path(OUTPUT_DIR) / mission_dir_name
 
-        print("Retrieving image file urls... (1 / 2)")
-        image_file_urls = get_data_dir_file_urls(
-            mission_dir_url, "DATA", FILE_NAME_FILTER, ".IMG"
-        )
+        img_dir_url = f"{mission_dir_url}/DATA"
 
-        print("Retrieving geometry file urls... (2 / 2)")
-        geometry_file_urls = get_data_dir_file_urls(
-            mission_dir_url, "GEOMETRY", FILE_NAME_FILTER, ".GEO"
-        )
+        orbit_dir_names = [
+            href.rstrip("/")
+            for href in get_hrefs(
+                img_dir_url, dir_only=True, negative_filters=[mission_dir_name]
+            )
+        ]
 
-        for image_file_url in image_file_urls:
-            print(image_file_url)
+        for orbit_dir_name in tqdm(
+            orbit_dir_names,
+            desc=f"└ Mission extension directory '{mission_dir_name}' progress",
+            leave=False,
+        ):
+            orbit_dir_url = f"{img_dir_url}/{orbit_dir_name}"
 
+            img_file_dir_path = mission_dir_path / "DATA" / orbit_dir_name
+            img_file_dir_path.mkdir(parents=True, exist_ok=True)
 
-def get_data_dir_file_urls(
-    mission_dir_url: str, data_dir_name: str, file_name_filter: str, file_extension: str
-) -> list[str]:
-    data_dir_url = f"{mission_dir_url}/{data_dir_name}"
+            geo_file_dir_path = mission_dir_path / "GEOMETRY" / orbit_dir_name
+            geo_file_dir_path.mkdir(parents=True, exist_ok=True)
 
-    data_orbit_dir_names = [
-        href.rstrip("/") for href in get_hrefs(data_dir_url, dir_only=True)
-    ]
+            img_file_names = get_hrefs(
+                orbit_dir_url, positive_filters=[FILE_NAME_FILTER, ".IMG"]
+            )
 
-    file_urls: list[str] = []
+            for img_file_name in tqdm(
+                img_file_names,
+                desc=f"  └ Orbit directory '{orbit_dir_name}' progress",
+                leave=False,
+            ):
+                img_file_url = f"{orbit_dir_url}/{img_file_name}"
 
-    for orbit_dir_name in tqdm(data_orbit_dir_names):
-        orbit_dir_url = f"{data_dir_url}/{orbit_dir_name}"
+                geo_file_name = img_file_name.rstrip(".IMG") + ".GEO"
+                geo_file_url = (
+                    f"{mission_dir_url}/GEOMETRY/{orbit_dir_name}/{geo_file_name}"
+                )
 
-        file_names = get_hrefs(
-            orbit_dir_url, href_filters=[file_name_filter, file_extension]
-        )
+                geo_file_response = requests.get(geo_file_url)
 
-        for file_name in file_names:
-            file_url = f"{orbit_dir_url}/{file_name}"
-            file_urls.append(file_url)
+                # If geometry file (url) does not exist skip downloads
+                if geo_file_response.status_code >= 400:
+                    continue
 
-    return file_urls
+                img_file_response = requests.get(img_file_url)
+
+                # If image file (url) does not exist skip downloads
+                if img_file_response.status_code >= 400:
+                    continue
+
+                img_file_path = img_file_dir_path / img_file_name
+                geo_file_path = geo_file_dir_path / geo_file_name
+
+                with open(img_file_path, "wb") as img_file:
+                    img_file.write(img_file_response.content)
+
+                with open(geo_file_path, "wb") as geo_file:
+                    geo_file.write(geo_file_response.content)
 
 
 def get_hrefs(
-    url: str, dir_only: bool = False, href_filters: list[str] | None = None
+    url: str,
+    dir_only: bool = False,
+    positive_filters: list[str] | None = None,
+    negative_filters: list[str] | None = None,
 ) -> list[str]:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -82,7 +109,12 @@ def get_hrefs(
         if dir_only and href[-1] != "/":
             continue
 
-        if href_filters is not None and not all(hf in href for hf in href_filters):
+        if positive_filters is not None and not all(
+            pf in href for pf in positive_filters
+        ):
+            continue
+
+        if negative_filters is not None and any(nf in href for nf in negative_filters):
             continue
 
         hrefs.append(href)
