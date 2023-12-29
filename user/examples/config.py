fddd182f63ceab27d6363f0725e5cf83ab4f86a5
Jan Mittendorf <jan.mittendorf@freenet.de>
2023-12-29 23:19:16 +0100

    Fix config path

--- user/examples/config.py
+++ user/examples/config.py
@@ -5,7 +5,7 @@ LOGS_DIR_PATH = Path("logs")
 DOWNLOADS_DIR_PATH = Path("datasets/satellite-datasets")
 SATELLITE_DATASET_TABLES_DIR_PATH = Path("datasets/satellite-datasets/tables")
 DOWNLOAD_CONFIGS_JSON_PATH = Path("datasets/download-configs.json")
-SATELLITE_DATASETS_JSON_PATH = Path("user-configs/satellite-datasets.json")
+SATELLITE_DATASETS_JSON_PATH = Path("user/satellite-datasets.json")
 
 # Misc
 DOWNLOAD_CHUNK_SIZE = 67_108_864  # 64 MB
