0b47bcfee76ce9512a0dc98bca2aac52b816c75f
Jan Mittendorf <jan.mittendorf@freenet.de>
2024-01-30 01:43:06 +0100

    Add patch dataset encoding transforms

--- source/patch_dataset/encoding.py
+++ source/patch_dataset/encoding.py
@@ -2,11 +2,12 @@ import typing
 
 import torch
 from numpy import ndarray
-from torch import no_grad
+from torch import Tensor, no_grad
+from torch.nn import Identity, Module
 from torch.utils.data import DataLoader
 from torchvision import models
 from torchvision.models import ResNet18_Weights
-from torch.nn import Identity
+from torchvision.transforms import Normalize, Resize, Compose
 
 import source.patch_dataset.config as pd_config
 
@@ -14,12 +15,25 @@ if typing.TYPE_CHECKING:
     from source.patch_dataset.dataset import PatchDataset
 
 
+class GrayscaleToRGB(Module):
+    def forward(self, input_tensor: Tensor):
+        return torch.cat([input_tensor] * 3, dim=-3)
+
+
 @no_grad()
 def encode_dataset(dataset: "PatchDataset") -> ndarray:
     model = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
     model.fc = Identity()  # type: ignore
 
+    transforms_list = [
+        Normalize(0.458971, 0.225609),
+        Resize(224, antialias=True),  # type: ignore
+        GrayscaleToRGB(),
+    ]
+    transforms = Compose(transforms_list)
+
     data_loader = DataLoader(dataset, batch_size=pd_config.ENCODING_BATCH_SIZE)
-    encoded_tensors = [model(batch_tensor) for batch_tensor in data_loader]
+
+    encoded_tensors = [model(transforms(batch_tensor)) for batch_tensor in data_loader]
 
     return torch.cat(encoded_tensors).numpy()
