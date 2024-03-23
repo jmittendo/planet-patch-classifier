41f5244834d8e0e2cf956169963882e17d007a1b
Jan Mittendorf <jan.mittendorf@freenet.de>
2024-03-23 14:47:09 +0100

    Remove resources dir path from config

--- source/config.py
+++ source/config.py
@@ -1,8 +1,5 @@
-from pathlib import Path
-
 import user.config as user_config
 
-RESOURCES_DIR_PATH = Path("resources")
 LOGS_DIR_PATH = user_config.DATA_DIR_PATH / "logs"
 DATA_DIR_PATH = user_config.DATA_DIR_PATH / "data"
 PLOTS_DIR_PATH = user_config.DATA_DIR_PATH / "plots"
